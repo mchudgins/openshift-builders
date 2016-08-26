6d6985c3a9684daed80a3563a9f79413c86e05a7
Mike Hudgins <mchudgins@dstsystems.com>
2016-08-26 08:48:04 -0500

    munge

--- golang/docker-entrypoint.sh
+++ golang/docker-entrypoint.sh
@@ -97,8 +97,8 @@ if [[ "$1" = 'build' ]]; then
 
 	if [ -n "${SOURCE_REF}" ]; then
 	  BUILD_DIR=$(mktemp --directory)
-		GIT_REPO=`echo ${SOURCE_REPOSITORY} | sed 's|^https://||' | sed 's|^http://||' | sed 's|^git://||' | sed 's|^git@||'`
-	  //git clone --recursive "${GIT_REPO}" "${BUILD_DIR}" >>/tmp/git.lis
+		GIT_REPO=`echo ${SOURCE_REPOSITORY} | sed 's|^https://||' | sed 's|^http://||' | sed 's|^git://||' | sed 's|^git@||' | sed 's|\.git$||'`
+	  #git clone --recursive "${SOURCE_REPOSITORY}" "${BUILD_DIR}" >>/tmp/git.lis
 		go get ${GIT_REPO}/...
 	  if [ $? != 0 ]; then
 	    echo "Error trying to fetch git source: ${SOURCE_REPOSITORY}"
