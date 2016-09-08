357f5dfe9a07de769c36790383d29bb10c51905c
Mike Hudgins <mchudgins@dstsystems.com>
2016-09-08 14:37:45 -0500

    tweaks to work with docker-1.12

--- hack-script.sh
+++ hack-script.sh
@@ -4,15 +4,20 @@ if [[ -z $1 ]]; then
 else
 	HOST=$1
 fi
+sudo docker pull debian:latest
 oc login --insecure-skip-tls-verify ${HOST}
 oc new-project mch
 oc import-image debian --from debian --confirm
 oc get is
 oc describe is/debian
-REGISTRY=`docker exec origin bash -c "oc get services" | awk '{if ( $1 == "docker-registry" ) { print $2 } }'`
-docker tag debian:latest ${REGISTRY}:5000/mch/debian:latest
-docker login -u mchudgins@dstsystems.com -e mchudgins@dstsystems.com -p `oc whoami -t` ${REGISTRY}:5000
-docker push ${REGISTRY}:5000/mch/debian:latest
+REGISTRY=`sudo docker exec origin bash -c "oc get services" | awk '{if ( $1 == "docker-registry" ) { print $2 } }'`
+if [[ -z "${REGISTRY}" ]]; then
+	echo "Unable to find Registry"
+	exit 1
+fi
+sudo docker tag debian:latest ${REGISTRY}:5000/mch/debian:latest
+sudo docker login -u mchudgins@dstsystems.com -e mchudgins@dstsystems.com -p `oc whoami -t` ${REGISTRY}:5000
+sudo docker push ${REGISTRY}:5000/mch/debian:latest
 oc new-app --file generic-builder-template.json -p IMAGESTREAM=golang,CONTEXT_DIR=/golang,GIT_REF=master,GIT_URI=https://github.com/mchudgins/openshift-builders.git,BASE_IMAGESTREAM=debian:latest
 oc start-build bc/golang
 oc logs --follow golang-1-build
